b352c7ca3ed904156acffb6a41317fb6a92f52b9
Matthias Hryniszak <padcom@gmail.com>
2023-06-08 14:56:52 +0200

    Added postcss with autoprefixer

--- src/components/HelloWorld.ce.vue
+++ src/components/HelloWorld.ce.vue
@@ -8,4 +8,10 @@ import { FormKit } from '@formkit/vue'
 
 <style>
 @import "./formkit-theme-ce.css";
+
+:host {
+  -webkit-text-stroke: 0.5px rgba;
+  -webkit-text-stroke: 0.5px green;
+  mask-mode: luminance;
+}
 </style>
