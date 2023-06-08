6a72f59c372f98e4adc290adebcbb083455266d3
Matthias Hryniszak <padcom@gmail.com>
2023-06-08 21:36:01 +0200

    Implemented ESLint

--- src/components/FormKitProvider.ce.vue
+++ src/components/FormKitProvider.ce.vue
@@ -10,7 +10,7 @@ import { createProPlugin, mask } from '@formkit/pro'
 const config = defaultConfig({
   plugins: [
     createProPlugin(import.meta.env.VITE_APP_FORMKIT_LICENSE, { mask }),
-  ]
+  ],
 })
 
 provide(optionsSymbol, config)
